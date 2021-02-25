8c5d4afd97bc2b992c91e6278354cfe3f36d5877
Michael Medina <michaelanthony.medinaa@gmail.com>
2021-02-25 09:21:15 -0800

    created component folders and added render framework in app.js

---
+++ employee-directory/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Jumbotron from "./components/jumbotron/";
+import Table from "./components/table";
+
+class App extends Component {
+
+
+
+  render() {
+    return (
+      <div>
+        <Jumbotron />
+        <Table />
+
+      </div>
+    )
+  }
+
+};
+
+export default App;
